92bc59f4cd18a64ca13b7026f9276c78a2952deb
Sigmanor <Sigmanor@users.noreply.github.com>
2015-01-12 20:07:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ define(function (require, exports, module) {
     'use strict';
     
     // How much the force is increased/decreased (at least) per scroll event.
-    var STEP = 0.00001;
+    var STEP = 0.00002;
     // How much the force is to be reduced each 16 ms (60fps). For instance, if the value is 0.8,
     // after 16ms the force will be 80% of what is was initially, after 32ms it will be 0.64% of
     // the initial value, and so on.
@@ -17,7 +17,7 @@ define(function (require, exports, module) {
     // friction.
     var PSEUDO_FRICTION = 0.93;
     // Maximum magnitude of the force that can be applied to the page.
-    var MAX_FORCE = 0.01;
+    var MAX_FORCE = 0.02;
     // If the speed of the page goes below this value, we just won't update the scroll position and
     // we will stop the page.
     var SPEED_THRESHOLD = 0.001;
